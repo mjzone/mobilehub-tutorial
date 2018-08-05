ebdf1b9d78de6e968f008910c906dfde45c5ef27
Manoj Fernando <mj.bitwave@gmail.com>
2018-08-06 05:16:29 +0530

    Revert changes

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { AmplifyService } from "aws-amplify-angular/lib/providers/amplify.service";
+import { AmplifyService } from "aws-amplify-angular";
 import { Router } from "@angular/router";
 
 @Component({
@@ -15,6 +15,7 @@ export class AuthComponent implements OnInit {
       if (authState.state === "signedIn") {
         this._router.navigateByUrl("/home");
       }
+      return false;
     });
   }
 
